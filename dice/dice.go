7e6fb142490b9ea2dc6c1fd18f6a7721f7ea619e
Salimvirji <Github@salim.virji.net>
2021-01-27 21:30:58 -0500

    Show the distribution of dice rolls
    Add an option to show the distribution of dice rolls.
    This might be better in perl ;)
    Using a Boolean flag, display a table of results.

--- dice/dice.go
+++ dice/dice.go
@@ -19,7 +19,7 @@ func rollDice() int {
         // only generate the randomness once
         onlyOnce.Do(func() {
                 rand.Seed(time.Now().UnixNano())
-        })
+        })  
 
         return dice[rand.Intn(len(dice))]
 }
@@ -27,6 +27,7 @@ func rollDice() int {
 func main() {
         numRolls := flag.Int("rolls", 5, "number of times to roll")
         numSides := flag.Int("sides", 6, "number of sides on each die")
+        printDistribution := flag.Bool("dist", false, "print distribution table")
         flag.Parse()
 
         // prepare the dice
@@ -35,16 +36,27 @@ func main() {
                         dice = []int{1, 2, 3, 4}
                 case 6:
                         dice = []int{1, 2, 3, 4, 5, 6}
-                case 10:
-                        dice = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-                case 12:
-                        dice = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+                case 10: 
+                        dice = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10} 
+                case 12: 
+                        dice = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12} 
                 default:
                         dice = []int{1, 2, 3, 4, 5, 6}
-        }
-        
+        }   
+
+        // keep a table of results
+        var dist = make([]int, *numSides+1) 
+
         for i := 1; i <= *numRolls; i++ {
-                diceroll := rollDice() 
+                diceroll := rollDice()
                 fmt.Printf("Die %3d: %2d\n", i, diceroll)
-        }
+                dist[diceroll]++
+        }   
+
+        if (*printDistribution ) { 
+                print 
+                for i :=1; i <= *numSides; i++ {
+                        fmt.Printf("%2d: %3d rolls\n", i, dist[i])
+                }   
+        }   
 }
